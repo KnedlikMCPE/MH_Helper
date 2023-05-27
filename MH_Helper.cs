0006ba2db616ac10f8b54c9e80b8ac8ff4b3f59a
KnedlikMCPE <knedlik@trainmazeland.com>
2023-05-27 20:05:17 +0200

    Add localisation support

--- MH_Helper.cs
+++ MH_Helper.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Astronautica;
 using BepInEx;
 using HarmonyLib;
 
 namespace MH_Helper
 {
-    [BepInPlugin("com.knedlik.mh.helper", "MH Helper", "1.0.0.0")]
+    [BepInPlugin("com.knedlik.mh.helper", "MH Helper", "1.1.0.0")]
     public class MH_Helper : BaseUnityPlugin
     {
         public static MH_Helper Instance;
         public static bool FirstTime;
         public Data data;
         public List<Action> onLoadDataActions = new List<Action>();
+        public Dictionary<string, Localisation.Entry> localeEntries;
 
         private void Awake()
         {
@@ -21,6 +23,10 @@ namespace MH_Helper
             // Plugin startup logic
             var harmony = new Harmony("com.knedlik.mh.helper");
             harmony.PatchAll();
+            
+            var entriesData = Localisation.instance.GetType()
+                .GetField("entriesDefault", BindingFlags.Instance | BindingFlags.NonPublic);
+            localeEntries = (Dictionary<string, Localisation.Entry>) entriesData.GetValue(Localisation.instance);
         }
     }
     
